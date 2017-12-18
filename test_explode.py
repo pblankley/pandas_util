46a5cf0c8f6ddea330cb6e2d45434a418f1626e3
pblankley <31221512+pblankley@users.noreply.github.com>
2017-12-18 13:12:07 -0600

    add test suite for explode

--- test_explode.py
+++ test_explode.py
@@ -5,10 +5,54 @@ Created on Tue Sep 19 09:29:53 2017
 
 @author: paulblankley
 """
-import pd_util as pu
 
-test = pd.DataFrame({'col1':[[1,7,4],[8,5,3]],'col2':['mike','larry'],'col3': \
-                     ['baby pig','llama'], 'col4':[['m','g','d'],['y','n','g']]})    
-test.head()
-dfo = pu.explode_value(test,'col2')
-dfo.head()
+# Need to work on this later
+import pd_util as pu
+import pandas as pd
+
+def test_2col():
+	test = pd.DataFrame({'col1':[[1,7],[8,5]],'col2':['mike','larry'],'col3': \
+                     ['baby pig','llama'], 'col4':[['m','g'],['y','n']]})
+	dfo = pu.explode_value(test,'col1','col4')
+	check = pd.DataFrame({'col1': [1,7,8,5],'col2': ['mike','larry','mike','larry'], 'col3': \
+				['baby pig','llama','baby pig','llama'],'col4': ['m','g','y','n']})
+	assert(all(dfo==check))
+
+def test_1col():
+	test = pd.DataFrame({'col1':[[1,7],[8,5]],'col2':['mike','larry'],'col3': \
+                     ['baby pig','llama']})
+	dfo = pu.explode_value(test,'col1')	
+	check = pd.DataFrame({'col1': [1,7,8,5],'col2': ['mike','larry','mike','larry'], 'col3': \
+				['baby pig','llama','baby pig','llama']})
+	assert(all(check==dfo))
+
+def test_notdf():	
+	check = [1,2,3,4,5,6,9,8,6]
+	try:
+		pu.explode_value(check, 'col1')
+	except ValueError as err:
+		assert(type(err)==ValueError)
+	
+def test_nocol():
+	test = pd.DataFrame({'col1':[[1,7],[8,5]],'col2':['mike','larry'],'col3': \
+                     ['baby pig','llama'], 'col4':[['m','g'],['y','n']]})
+	try:
+		pu.explode_value(test,'no_col_here')
+	except KeyError as err:
+		assert(type(err)==KeyError)
+
+def test_collen():
+	test = pd.DataFrame({'col1':[[1,7],[8,5]],'col2':['mike','larry'],'col3': \
+                     ['baby pig','llama'], 'col4':[['m','g','g'],['y','n','e']]})
+	try:
+		pu.explode_value(test,'col1','col2')
+	except ValueError as err:
+		assert(type(err)==ValueError)
+
+
+test_2col()
+test_1col()
+test_notdf()	
+test_nocol()
+test_collen()
+
